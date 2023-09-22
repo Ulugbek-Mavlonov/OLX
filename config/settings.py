0c936f835bd8346762351dd4a8f5a5c986cb6e0d
gitignore.py <126414760+Rustambek571@users.noreply.github.com>
2023-09-22 10:53:15 -0700

    general info app all

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'general_info_app',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- general_info_app/models.py
+++ general_info_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Address(models.Model):
+    company_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.company_name
+
+
+
+class Socialnetwork(models.Model):
+    link = models.URLField(max_length=200)
+    code = models.IntegerField()
+
+    def __str__(self):
+        return self.link
+
+
+class Faqs(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.TextField()
+
